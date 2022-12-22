d3f006d287a41165859c451915897ce680393947
Mohamed Salah <mohamed_salah@rayacx.com>
2022-12-22 17:38:05 +0200

    Add project files.

---
+++ RDLCReportApp/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Reporting.NETCore;
+using Microsoft.ReportingServices.Interfaces;
+using RDLCReportApp.Models;
+using System.Data;
+using System.Text;
+
+namespace RDLCReportApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public SalaryService salaryService = new();
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment webHostEnvironment)
+        {
+            _logger = logger;
+            _webHostEnvironment = webHostEnvironment;
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult EmployeeSalaryInfo()
+        {
+
+            var dataTable = new DataTable();
+
+            dataTable = salaryService.GetActorInfo();
+
+            var mineType = "";
+            var extension = 1;
+            var path = $"{_webHostEnvironment.WebRootPath}\\Reports\\rptActorInfo.rdlc";
+            Stream reportDefinition;
+            using var fs = new FileStream(path, FileMode.Open);
+            reportDefinition = fs;
+
+            var report = new LocalReport();
+            report.LoadReportDefinition(reportDefinition);
+
+            report.DataSources.Add(new ReportDataSource("source", dataTable));
+          
+            byte[] pdf = report.Render("PDF");
+            fs.Dispose();
+
+            return File(pdf, "application/pdf", "test.pdf");
+
+
+
+
+
+        }
+    }
+}
---
+++ RDLCReportApp/Models/SalaryService.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace RDLCReportApp.Models
+{
+    public class SalaryService
+    {
+        //string constr = @"Data Source=DESKTOP-T64S8Q3;Initial Catalog=Movies;User ID=MooSalah;Password=12346";
+        //string constr = @"Data Source=DESKTOP-T64S8Q3;Integrated Security=true;";
+        string constr = @"Data Source=DESKTOP-T64S8Q3;Database=Movies;Trusted_Connection=True;";
+
+        public DataTable GetActorInfo()
+        {
+            var dt = new DataTable();
+            using (SqlConnection con = new(constr))
+            {
+                var cmd = new SqlCommand("SPGetActorInfo", con)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                con.Open();
+
+                var dataAdapter = new SqlDataAdapter(cmd);
+                dataAdapter.Fill(dt);
+
+                con.Close();
+
+            }
+            return dt;
+        }
+
+
+    }
+}
